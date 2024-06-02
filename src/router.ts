7a87f119a1c9c4ca3634e0f20ace229676f15d76
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-06-02 14:56:21 -0300

    create useCases listOrders.ts and createOrder.ts

--- src/router.ts
+++ src/router.ts
@@ -10,6 +10,8 @@ import { listProducts } from './app/useCases/products/listProducts';
 import { createProduct } from './app/useCases/products/createProduct';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
 import { deleteProduct } from './app/useCases/products/deleteProduct';
+import { listOrders } from './app/useCases/orders/listOrders';
+import { createOrder } from './app/useCases/orders/createOrder';
 
 export const router = Router();
 
@@ -46,14 +48,10 @@ router.get('/categories/:categoryId/products', listProductsByCategory);
 router.delete('/products/:productId', deleteProduct);
 
 // LIST ORDERS
-router.get('/orders', (req, res) => {
-  res.send('OK');
-});
+router.get('/orders', listOrders);
 
 // CREATE ORDER
-router.post('/orders', (req, res) => {
-  res.send('OK');
-});
+router.post('/orders', createOrder);
 
 // CHANGE ORDER STATUS
 router.patch('/orders/:orderId', (req, res) => {
