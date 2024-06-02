98279791a144306193c989845e13de1c1016c209
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-06-02 12:36:55 -0300

    update multer function in router.ts

--- src/router.ts
+++ src/router.ts
@@ -14,7 +14,10 @@ const upload = multer({
   storage: multer.diskStorage({
     destination(req, file, callback) {
       callback(null, path.resolve(__dirname, '..', 'uploads'));
-    }
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`);
+    },
   }),
 });
 
