ab3905584ad0915f74cc6d97432c0961fecd7f1c
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-06-02 12:16:49 -0300

    add multer and @types/multer

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { listCategories } from './app/useCases/categories/listCategories';
 import { createCategory } from './app/useCases/categories/createCategory';
